7e4e8073580647f3cfee6610a851ebf2bc50642f
xdany01 <42984156+xdany01@users.noreply.github.com>
2022-04-16 12:12:44 -0300

    mejoras de codigo

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,7 @@
 let items = [];
-let list = document.getElementById('list');
-let ingredientesOcultos = document.getElementById('ingredientesOcultos');
-const variantes = {
-    'gray': 'gray',
-    'trueGray': 'true-gray',
-    'blueGray': 'blue-gray',
-    'rose': 'rose',
-    'red': 'red',
-    'pink': 'pink',
-    'teal': 'teal',
-    'emerald': 'emerald',
-    'green': 'green',
-    'lime': 'lime',
-    'yellow': 'yellow',
-    'amber': 'amber',
-    'blue': 'blue',
-    'lightBlue': 'light-blue',
-    'cyan': 'cyan',
-    'indigo': 'indigo',
-    'fuchsia': 'fuchsia',
-    'purple': 'purple',
-    'violet': 'violet'
-}
+let list = document.querySelector('ul[data-dn-list-items]');
+let itemsOcultos = document.querySelector('input[data-dn-items-ocultos]');
+let varianteColor = document.querySelector('input[data-dn-color]').getAttribute('data-dn-color');
 
 const ready = function (cb) {
     document.readyState === "loading" ? document.addEventListener("DOMContentLoaded", function (e) {
@@ -34,13 +14,13 @@ const focusEle = (element) => {
 const remove = (i) => {
     items = items.filter(item => items.indexOf(item) !== i);
     render();
-    focusEle('#txt');
+    focusEle('input[data-dn-input-text]');
 }
 const chip = (item, index, style) => {
     return `<li class="d-inline-block ${style}">${item}<a href="javascript:remove(${index})">&times;</a></li>`;
 }
 const fnAlert = (msj, tipo) => {
-    return `<div id="dnAlert" class="alert alert-${tipo} alert-dismissible fade show position-absolute top-0 end-0 mt-5 me-5" role="alert">
+    return `<div data-dn-alert="dnAlert" class="alert alert-${tipo} alert-dismissible fade show position-absolute top-0 end-0 mt-5 me-5" role="alert">
                 ${msj}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>`;
@@ -55,27 +35,27 @@ const hideAlert = (alertDOM) => {
 const showAlert = (adjacent, msj, tipo) => {
     let alert = fnAlert(msj, tipo);
     adjacent.insertAdjacentHTML("afterbegin", alert);
-    let alertDOM = document.querySelector('#dnAlert');
+    let alertDOM = document.querySelector('div[data-dn-alert]');
     hideAlert(alertDOM);
 }
-const render = () => {
+const render = (varianteColor) => {
     list.innerHTML = '';
     items.map((item, index) => {
-        list.innerHTML += chip(item, index, variantes.green);
+        list.innerHTML += chip(item, index, varianteColor);
     });
-    ingredientesOcultos.value = items.toString();
-    list.insertAdjacentHTML('beforeend', `<li class="d-inline-block"><input autocomplete="off" class="input-txt" id="txt" type="text"></li>`);
+    itemsOcultos.value = items.toString();
+    list.insertAdjacentHTML('beforeend', `<li class="d-inline-block"><input autocomplete="off" class="input-txt" data-dn-input-text="txt" type="text"></li>`);
 }
 document.addEventListener('keydown', (e) => {
-    if (e.target.matches('#txt')) {
+    if (e.target.matches('input[data-dn-input-text]')) {
         let txt = e.target;
         let val = txt.value.trim();
         if (e.key === 'Backspace') {
             if (val === '') {
                 items.pop();
-                render();
+                render(varianteColor);
                 txt.value = '';
-                focusEle('#txt');
+                focusEle('input[data-dn-input-text]');
             }
         }
         if (e.key === 'Enter') {
@@ -83,25 +63,25 @@ document.addEventListener('keydown', (e) => {
             if (val !== '') {
                 if (items.indexOf(val) >= 0) {
                     showAlert(body, 'El item esta repetido.', 'warning');
-                    document.querySelector('#txt').focus();
+                    focusEle('input[data-dn-input-text]');
                 } else {
                     items.push(val);
-                    render();
+                    render(varianteColor);
                     txt.value = '';
-                    focusEle('#txt');
+                    focusEle('input[data-dn-input-text]');
                 }
             } else {
                 showAlert(body, 'Ingrese un item porfavor.', 'info');
-                focusEle('#txt');
+                focusEle('input[data-dn-input-text]');
             }
         }
     }
 });
 document.addEventListener('click', (e) => {
-    if (e.target.matches('#dnFormControl')) {
-        focusEle('#txt');
+    if (e.target.matches('div[data-dn-form-control]') || e.target.matches('ul[data-dn-list-items]') || e.target.matches('input[data-dn-items-ocultos] ~ label')) {
+        focusEle('input[data-dn-input-text]');
     }
 });
 ready(function () {
-    render();
+    render(varianteColor);
 });
